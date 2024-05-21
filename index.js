148e4c4816e36604003f02434edc6bde42ae2ebe
SMNoushan <noushan.md@gmail.com>
2024-05-21 13:54:31 +0600

    added db connection

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const express = require("express");
@@ -23,6 +24,30 @@ const cookieOption = {
   sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
   secure: process.env.NODE_ENV === "production" ? true : false,
 };
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.b6wqjn1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    console.log("Successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+    // console.log(
+    //   "Close your deployment. You successfully terminate connection to MongoDB!"
+    // );
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("BistroBoss Server RUNNING");
